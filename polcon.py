c8b7fe803091088ffc25f2fd8f3efb092f7bfa5b
Damian Melniczuk <damian.melniczuk@gmail.com>
2013-12-13 11:12:01 +0100

    cool plots

--- polcon.py
+++ polcon.py
@@ -3,51 +3,57 @@ import sys
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import subprocess
+import numpy as np
+import random
+
 
 
 TIMEOUT_SEC = 0.250
-args = sys.argv[1:]
-
-# First set up the figure, the axis, and the plot element we want to animate
-fig = plt.figure()
-ax = plt.axes()
+#args = sys.argv[1:]
+starting_time = time.time()
 x, y = [], []
-line, = ax.plot([], [])
 
-# initialization function: plot the background of each frame
-def init():
-    line.set_data([], [])
-    return line,
-    
-def animate(i):
-        
-	# create and call process, store output and error in "o" and "e"
+plt.ion()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+fig.canvas.set_window_title('broken spiral')
+line1, = ax.plot([], [],'-k',label='black')
+ax.legend()
+ax.grid()
+
+
+while True:
+	"""# create and call process, store output and error in "o" and "e"
 	p = subprocess.Popen(args, stdout = subprocess.PIPE)
 	o, e = p.communicate()
 	
-	# show results to user
-	#sys.stdout.write(str(time.time()) + ": " + o)
+	splitted = o.split(";")"""
+	summe = 0
+	for cnt in range(0, 4):
+		splitted=[10*random.random()]
+		summe = summe + splitted[0] 
+		time.sleep(0.25)
 	
-	splitted = o.split(";")
+	x.append(time.time()-starting_time)
+	y.append(summe)
 	
-	
-	x.append(time.time())
-	y.append(splitted[0])
+	print x, y
 	
 	length_of_list = len(x)
-	if length_of_list > 100:
+	if length_of_list > 60:
 		x.pop(0)
 		y.pop(0)
 	
-	print x, y
-	line.set_data(x, y)
-
-	ax.set_autoscale_on(True)
-	ax.autoscale_view(True,True,True)
+	
+		
+	
+	line1.set_xdata(x)
+	line1.set_ydata(y)
 	ax.relim()
-	return line,
+	ax.autoscale_view()
+	
+	plt.draw()
+	
 
-# call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init, interval=250, blit=True)
-plt.grid()
-plt.show()
+
